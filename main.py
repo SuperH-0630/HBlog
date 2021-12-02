7598f33f92498aec78917310b93b81a312c61e7d
SongZihuan <jimsong2010@163.com>
2021-12-02 23:11:27 +0800

    feat: 使用waitress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from view import WebApp
+from waitress import serve
 
 
 web = WebApp(__name__)
@@ -6,4 +7,5 @@ app = web.get_app()
 
 
 if __name__ == '__main__':
-    app.run()
+    print("已启动服务: 127.0.0.1:8080")
+    serve(app, host='0.0.0.0', port="8080")
