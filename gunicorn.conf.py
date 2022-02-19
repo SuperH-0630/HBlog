e8932370e29c104ad4bebb62b04425f2a65dab65
SongZihuan <jimsong2010@163.com>
2022-02-19 21:27:39 +0800

    fix: 修复gunicorn路径配置错误的问题

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,17 +5,15 @@ import multiprocessing
 import logging.handlers
 import logging
 
-bind = '0.0.0.0:80'
+bind = '127.0.0.1:5000'
 timeout = 30  # 超时
 
 worker_class = 'gevent'
 workers = multiprocessing.cpu_count() * 2 + 1  # 进程数
 threads = 2  # 指定每个进程开启的线程数
 
-loglevel = 'info'
-access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'
-
-os.makedirs("~/hblog", exist_ok=True, mode=0o775)
+hblog_path = os.path.join(os.environ['HOME'], "hblog")
+os.makedirs(hblog_path, exist_ok=True, mode=0o775)
 
 # 设置访问日志和错误信息日志路径
 log_format = ("[%(levelname)s] %(name)s %(asctime)s "
@@ -33,7 +31,7 @@ gunicorn_error_logger.setLevel(logging.WARNING)
 
 gunicorn_error_logger.addHandler(console_handle)
 
-errorlog = "~/hblog/gunicorn_error.log"
+errorlog = os.path.join(hblog_path, "gunicorn_error.log")
 time_handle = logging.handlers.TimedRotatingFileHandler(errorlog, when="d", backupCount=30, encoding='utf-8')
 gunicorn_error_logger.addHandler(time_handle)
 time_handle.setFormatter(log_formatter)
@@ -44,7 +42,16 @@ gunicorn_access_logger.setLevel(logging.INFO)
 
 gunicorn_access_logger.addHandler(console_handle)
 
-accesslog = "~/hblog/gunicorn_access.log"
+accesslog = os.path.join(hblog_path, "/gunicorn_access.log")
 time_handle = logging.handlers.TimedRotatingFileHandler(accesslog, when="d", backupCount=10, encoding='utf-8')
 gunicorn_access_logger.addHandler(time_handle)
 time_handle.setFormatter(log_formatter)
+
+# 输出日志
+gunicorn_access_logger.info("Load gunicorn conf success")
+gunicorn_access_logger.info(f"bind: {bind}")
+gunicorn_access_logger.info(f"timeout: {timeout}")
+gunicorn_access_logger.info(f"worker_class: {worker_class}")
+gunicorn_access_logger.info(f"workers: {workers}")
+gunicorn_access_logger.info(f"threads: {threads}")
+gunicorn_access_logger.info(f"hblog_path: {hblog_path}")
