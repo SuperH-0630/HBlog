4dc526d5c03f76bb66648361a8a8a575ac752726
SongZihuan <songzihuan@song-zh.com>
2022-10-28 09:25:54 +0800

    feat: 自定义sender

--- send_email/__init__.py
+++ send_email/__init__.py
@@ -5,7 +5,7 @@ from configure import conf
 
 def send_msg(title: str, mail: Mail, to, template, **kwargs):
     """ 邮件发送 """
-    sender = f"HBlog Admin <{conf['MAIL_SENDER']}>"
+    sender = conf['MAIL_SENDER']
     message = Message(conf['MAIL_PREFIX'] + title, sender=sender, recipients=[to])
     message.body = render_template("email-msg/" + template + ".txt", **kwargs)
     message.html = render_template("email-msg/" + template + ".html", **kwargs)
